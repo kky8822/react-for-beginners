9b9cbc326860229e51d79342cfda030533d9cb46
kky8822 <99176588+kky8822@users.noreply.github.com>
2022-06-02 08:21:26 +0900

    220602_ToDos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
 
-  function Hello() {
-    function destroied() {
-      console.log("destroied :(");
-    }
-    function create() {
-      console.log("created :)");
-      return destroied;
-    }
-    useEffect(create, []);
-    return <h1>Hello</h1>;
-  }
+  useEffect(() => console.log(toDos), [toDos]);
 
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to Do"
+          value={toDo}
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
